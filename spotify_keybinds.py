20a98295655bf9d3aa5d85c4080e6f0074527d2d
Serrindipity <jon.quitoriano@gmail.com>
2024-05-06 16:41:01 -0700

    add next/prev track functionality and verbosity level support

--- spotify_keybinds.py
+++ spotify_keybinds.py
@@ -3,19 +3,38 @@ from pynput import keyboard
 
 keyb = keyboard.Controller()
 
+# Which print statements should be displayed?
+verbosity = 1
+
+def check_print(statement, verbosity_level=1):
+    if verbosity == verbosity_level:
+        print(statement)
+
+def press_vk_key(keycode):
+    keyb.press(key = keyboard.KeyCode.from_vk(keycode))
+
+
+# Functions to run when keybinds are pressed
 def on_activate_play():
-    print('Play/Pause activated')
-    keyb.press(key = keyboard.KeyCode.from_vk(179))
+    check_print('Play/Pause activated')
+    press_vk_key(179)
 
-def for_canonical(f):
-    return lambda k: f(l.canonical(k))
+def prev_track():
+    check_print('Previous Track')
+    press_vk_key(177)
 
+def next_track():
+    check_print('Next Track')
+    press_vk_key(176)
+    
 def quit():
-    print("Quitting.")
+    check_print("Quitting.")
     exit()
 
 with keyboard.GlobalHotKeys({
-        '<ctrl>+<alt>+p': on_activate_play,
+        '<alt>+p': on_activate_play,
+        '<alt>+j': prev_track,
+        '<alt>+k': next_track,
         '<ctrl>+<alt>+q': quit,
         }) as h:
     h.join()
