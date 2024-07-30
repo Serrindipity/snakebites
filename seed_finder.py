b94744218cea23c09ef4d030b58cda80891f9797
Serrindipity <jon.quitoriano@gmail.com>
2024-07-29 18:14:50 -0700

    refactor get_stat_files to return both kill and stat files

--- seed_finder.py
+++ seed_finder.py
@@ -1,12 +1,7 @@
 des = "A tool for Noita that takes the path to a sessions folder and returns data based on a query"
 import argparse
 from pathlib import Path
-from enum import StrEnum
 import xml.etree.ElementTree as ET
-
-class stat_types(StrEnum):
-      STATS = "stats"
-      KILLS = "kills"
 
 # Possible search values:
 # Stats: items, hp, gold_all, places_visited, projectiles_shot
@@ -17,18 +12,22 @@ def make_parsed_args():
         parser = argparse.ArgumentParser(prog = f"{Path(__file__).name}", description=des)
         parser.add_argument("-f", "--folder", type=str, help = "Filepath to session folder", default = r'c:\Users\jonqu\AppData\LocalLow\Nolla_Games_Noita\save00\stats\sessions')
         parser.add_argument('-l', "--limit", type=int, help = "Maximum number of seeds to return", default = 3)
-        parser.add_argument('-t', '--type', type=str, help="Type of stat file to parse. Must be in ALL CAPS", default = "STATS")
         parser.add_argument('-s', '--search', type=str, help="Type of thing to look for")
         
         parsed = parser.parse_args()
-        assert parsed.type.lower() in stat_types
         return parsed
 
-# Returns a list of Path Objects to xml files
-def get_stat_files(dir_path, stat_type = stat_types.STATS):
-      assert stat_type in stat_types
+# Returns a list of tuples, of the form (kills file path, stats file path) of Path Objects to xml files
+def get_stat_files(dir_path):
       sessions = Path(dir_path)
-      return [path for path in sessions.iterdir() if stat_type + ".xml" in str(path)]
+      out = []
+      for kills_file in sessions.glob('*_kills.xml'):
+           filename = str(kills_file).split("sessions\\")[1]
+           serial = filename.split("_kills")[0]
+           stats_file = sessions.glob(f'*{serial}_stats.xml').__next__()
+           zipped = (kills_file, stats_file)
+           out.append(zipped)
+      return out
 
 # Parses a kill or stat xml document, takes a pathlib argument. Returns search value, seed
 def parse_xml(file, search):
@@ -56,6 +55,7 @@ if __name__ == "__main__":
     print("running")
     args = make_parsed_args()
     files_to_parse = get_stat_files(args.folder)
-    for file in files_to_parse:
-         parse_xml(file, args.search)
-         break
+    print(files_to_parse)
+    # for file in files_to_parse:
+    #      parse_xml(file, args.search)
+    #      break
